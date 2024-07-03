b9be4388c0b79980afc5743cbef3a3e760fbefb5
Marco Emilio Vazquez <maremivazquez@ufl.edu>
2024-07-03 14:55:44 -0400

    added post example and index.html

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Flex Fit!'
+    # return 'Welcome to Flex Fit!'
+    return render_template('index.html')
+
+# Define a route for handling the form submission
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    return f'Hello, {name}, Welcome to Flex Fit!'
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
