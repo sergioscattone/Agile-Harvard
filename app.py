cb7e278deb4b18909293e2a397afadbf4b80777d
Marco Emilio Vazquez <maremivazquez@ufl.edu>
2024-07-03 20:02:12 -0400

    added more tests and details page

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+# Define a route for handling the form submission
+@app.route('/exercise', methods=['POST'])
+def exercise_form():
+    # name = request.form['name']
+    sites = ["Bicep Curl", "Jacknife Situps"]
+    return render_template("exercises.html", sites=sites)
+
+
+@app.route('/site/<site_name>')
+def site_detail(site_name):
+    # Replace spaces with underscores in site_name
+    site_name_clean = site_name.replace(" ", "_")
+    return render_template('exercise_details.html', site_name=site_name_clean)
+
+
 @app.route('/')
 def hello_world():
     # return 'Welcome to Flex Fit!'
     return render_template('index.html')
 
-# Define a route for handling the form submission
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    name = request.form['name']
-    return f'Hello, {name}, Welcome to Flex Fit!'
 
 if __name__ == "__main__":
    app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,11 +11,49 @@ def client():
         yield client
 
 
+def get_context_variable(response_data, variable_name):
+    # Helper function to extract a variable from the template context
+    start = response_data.find(b'{{') + 2
+    end = response_data.find(b'}}')
+    variable = response_data[start:end].strip()
+    return variable
+
+
 def testing_framework_works():
     assert True
 
 
-def test_welcome(client):
+def test_welcome_page(client):
     """Test welcome page"""
     rv = client.get('/')
+    assert rv.response == 200
+
+
+def test_welcome_title(client):
+    """Test welcome title"""
+    rv = client.get('/')
     assert b'Welcome' in rv.data
+
+
+# check submit page exists
+def test_exercise_page(client):
+    """Test list page"""
+    rv = client.post('/exercise')
+    assert rv.status_code == 200
+
+
+# check that there is at least 1 exercise in the list
+def test_check_exercises_number(client):
+    rv = client.post('/exercise')
+    sites = get_context_variable(rv.data, b'sites')
+    assert len(sites) >= 1
+
+
+def test_exercise_bicep_curl_details_page(client):
+    rv = client.get('/site/Bicep_Curl')
+    assert rv.status_code == 200
+
+
+def test_exercise_jackknife_situps_details_page(client):
+    rv = client.get('/site/Jacknife_Situps')
+    assert rv.status_code == 200
