51edab13b528983aaef95f257f74d5e4fbfd1881
Marco Emilio Vazquez <maremivazquez@ufl.edu>
2024-07-11 13:41:37 -0400

    added more exercise descriptions

--- exercise.py
+++ exercise.py
@@ -35,10 +35,10 @@ class Exercise():
 Bicep_Curl = Exercise(1, 'Bicep Curl', 'Curling exercise working the biceps brachii muscle where the elbow is stationary and the arm slowly bends from an overstretched position to a folded one.')
 Jacknife_Situps = Exercise(2, 'Jacknife Situps', 'Exercise where a person laying on a flat surface folds their body such that the tip of their toes touches the tip of the fingers.')
 Swimming = Exercise(3, 'Swimming', 'A popular recreational exercise that involves moving in a body of water. Useful for cardio and training of the whole body, with an emphasis on upper body.')
-Jogging = Exercise(4, 'Jogging', 'TODO')
-Hiking = Exercise(5, 'Hiking', 'Balabala')
-Table_Tennis = Exercise(6, 'Table Tennis', 'Balabala')
-More = Exercise(7, 'More', 'Balabala')
+Jogging = Exercise(4, 'Jogging', 'A form of running at a specific pace, typically done for cardio exercise. This will primarily focus on cardiovascular and leg endurance.')
+Hiking = Exercise(5, 'Hiking', 'An outdoor activity that involves walking up trails or natural paths, typically on an incline. This is a leisure activity done for cardio exercise and focuses primarily on legs.')
+Table_Tennis = Exercise(6, 'Table Tennis', 'Table tennis, often known as ping pong, is a sport played indoors on a special table.')
+More = Exercise(7, 'More', 'Placeholder section to add more exercise')
 exercises: List[Exercise] = [
     Bicep_Curl,
     Jacknife_Situps,
